db183d840bc17ba87ac71ecb7730a0b3f4969577
Firas Moosvi <firas@moosvi.com>
2019-09-20 23:24:52 -0700

    yak shaving by writing a quick script to return your labs.

---
+++ Final revisions/getShortGitSHA.sh
@@ -0,0 +1,9 @@
+# This is a short script to get the short SHA-1 from sourcetree as the long one is super annoying. 
+# To use this, you need to run chmod +x to give it execute priveleges and then add it as a script in the SourceTree custom actions.
+# parameters can be left empty. 
+## P.S. pbcopy copies it to clipboard
+
+
+#!/bin/bash
+
+git rev-parse --short HEAD | /usr/bin/pbcopy
